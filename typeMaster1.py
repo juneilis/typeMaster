6772fc4d9bcfec172bec0aa4fb77d4b03a29c941
juneilis <juneilis.hernandez02@gmail.com>
2025-02-12 13:38:39 -0400

    labels

--- typeMaster1.py
+++ typeMaster1.py
@@ -10,3 +10,15 @@ def main(page: ft.Page):
     index = 0 
     errores = 0
     palabrasTotales = len(palabras)
+
+    palabraLabel = ft.Text(palabras[index], size=16)
+    estadoLabel = ft.Text("", size=20)
+    aciertosLabel = ft.Text("Precisión: 100.00%", size=16)
+    progresoLabel = ft.Text(f"Progreso: {index}/{palabrasTotales}", size=16)
+
+    page.update()
+
+    page.add(palabraLabel, estadoLabel, aciertosLabel, progresoLabel)
+
+ft.app(target=main)
+
