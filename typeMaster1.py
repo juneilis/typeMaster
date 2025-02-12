8a00cd3ccdff26f4577b653c184bbcbad5f90053
juneilis <juneilis.hernandez02@gmail.com>
2025-02-12 14:02:46 -0400

    agg lo de correcto e incorrecto

--- typeMaster1.py
+++ typeMaster1.py
@@ -16,9 +16,30 @@ def main(page: ft.Page):
     aciertosLabel = ft.Text("Precisión: 100.00%", size=16)
     progresoLabel = ft.Text(f"Progreso: {index}/{palabrasTotales}", size=16)
 
-    page.update()
+    input1 = ft.TextField(on_submit=lambda e: chequearPalabra(e, page))
 
-    page.add(palabraLabel, estadoLabel, aciertosLabel, progresoLabel)
+    def chequearPalabra(e, page):
+        nonlocal index, errores
+        if input1.value == palabras[index]:
+            estadoLabel.value = "Correcto"
+            estadoLabel.color = "green"
+        else: 
+            estadoLabel.value = "Incorrecto"
+            estadoLabel.color = "red"
+            errores +=1
+        index +=1
+        if index < palabrasTotales:
+            palabraLabel.value = palabras[index]
+            progresoLabel.value = f"Progreso: {index}/{palabrasTotales}" 
+            input1.value = ""
+        else:
+            aciertos = ((palabrasTotales - errores)/ palabrasTotales) * 100
+            aciertosLabel.value = f"Precisión: {aciertos: .2f}% (Errores: {errores})" 
+            input1.disabled = True
+        
+        page.update()
+
+    page.add(palabraLabel, input1,estadoLabel, aciertosLabel, progresoLabel)
 
 ft.app(target=main)
 
